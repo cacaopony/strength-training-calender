264666b8d613442d24ddcb753b1a56aa3c6e9932
Cacao Fumiya <shikoku.fumiya@gmail.com>
2024-05-05 16:25:48 +0900

    add event, date click action, event click action

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+import './App.css'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-
 // import timeGridPlugin from '@fullcalendar/timegrid'
+// import jaLocale from '@fullcalendar/core/locales/ja';
+//日本語表記
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 
 export default function Calendar() {
+  const handleDateClick = (arg) => {
+    alert(arg.dateStr)
+  }
+  //日付をクリックすると日付をalertする関数
+  const handleEventClick = (arg) => {
+    alert(arg.event.title)
+  }
+  //イベントをクリックするとイベントのタイトルをalertする関数
   return (
     <FullCalendar
-      plugins={[dayGridPlugin]} 
+      plugins={[dayGridPlugin, interactionPlugin]}
       initialView="dayGridMonth"
       weekends={true}
       events={[
-        { title: '腕', date: '2024-05-01' },
-        { title: '肩', date: '2024-05-20' }
-      ]}
+        { title: '腕を休める期間', start: '2024-05-01', end: '2024-05-03' },
+        { title: '太ももを鍛える日', date: '2024-05-02' }
+      ]
+      }
+      dateClick={handleDateClick}
+      eventClick={handleEventClick}
+    // locales={[jaLocale]}
+    //日本語表記
     />
   )
 }
