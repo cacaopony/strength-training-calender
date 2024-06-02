23345dfc1542adbd88a71241e0801192081291be
Cacao Fumiya <shikoku.fumiya@gmail.com>
2024-06-02 18:26:08 +0900

    サイドバーが表示されない...

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 // import jaLocale from '@fullcalendar/core/locales/ja';
 //日本語表記
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import Sidebar from './Sidebar'
 
 
 export default function App() {
@@ -18,21 +19,24 @@ export default function App() {
   //イベントをクリックするとイベントのタイトルをalertする関数
 
   let contentHeight = '800px'; //カレンダーの高さ
-  
+
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-      weekends={true}
-      events={[
-        { title: '腕を休める期間', start: '2024-05-01', end: '2024-05-03' },
-        { title: '太ももを鍛える日', date: '2024-05-02' }
-      ]}
-      contentHeight={contentHeight}
-      dateClick={handleDateClick}
-      eventClick={handleEventClick}
-    // locales={[jaLocale]}
-    //日本語表記
-    />
+    <div>
+      <Sidebar></Sidebar>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        weekends={true}
+        events={[
+          { title: '腕を休める期間', start: '2024-05-01', end: '2024-05-03' },
+          { title: '太ももを鍛える日', date: '2024-05-02' }
+        ]}
+        contentHeight={contentHeight}
+        dateClick={handleDateClick}
+        eventClick={handleEventClick}
+      // locales={[jaLocale]}
+      //日本語表記
+      />
+    </div>
   )
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+export default function Sidebar(){
+    <div>
+        <ul>
+            <li>
+                カレンダー
+            </li>
+            <li>
+                項目１
+            </li>
+        </ul>
+    </div>
+}
