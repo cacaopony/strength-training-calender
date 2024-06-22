8dace4322cf261c6fefd01f117e3922fd6beca61
Cacao Fumiya <shikoku.fumiya@gmail.com>
2024-06-22 17:22:27 +0900

    useState練習

--- src/App.js
+++ src/App.js
@@ -5,27 +5,33 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 // import jaLocale from '@fullcalendar/core/locales/ja';
 //日本語表記
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import { useState } from 'react'
+
+
+const handleDateClick = (arg) => {
+  alert(arg.dateStr)
+}
+//日付をクリックすると日付をalertする関数
+const handleEventClick = (arg) => {
+  alert(arg.event.title)
+}
+//イベントをクリックするとイベントのタイトルをalertする関数
+
+let contentHeight = '800px'; //カレンダーの高さ
 
 
 export default function App() {
-  const handleDateClick = (arg) => {
-    alert(arg.dateStr)
-  }
-  //日付をクリックすると日付をalertする関数
-  const handleEventClick = (arg) => {
-    alert(arg.event.title)
-  }
-  //イベントをクリックするとイベントのタイトルをalertする関数
-
-  let contentHeight = '800px'; //カレンダーの高さ
-
-  
+const [isCalendar, setIsCalendar] = useState(true)
+const clickCalendar = () => {
+  setIsCalendar(!isCalendar);
+  // alert(`Click!!!!!!`);
+}
 
   return (
     <div className='container'>
       <div className="side-bar">
         <div>
-          <div>
+          <div onClick={clickCalendar}>
             カレンダー
           </div>
           <div>
@@ -33,23 +39,35 @@ export default function App() {
           </div>
         </div>
       </div>
-      <div className='calendar'>
-        <FullCalendar
-          plugins={[dayGridPlugin, interactionPlugin]}
-          initialView="dayGridMonth"
-          weekends={true}
-          events={[
-            { title: '腕を休める期間', start: '2024-05-01', end: '2024-05-03' },
-            { title: '太ももを鍛える日', date: '2024-05-02' }
-          ]}
-          contentHeight={contentHeight}
-          dateClick={handleDateClick}
-          eventClick={handleEventClick}
-        // locales={[jaLocale]}
-        //日本語表記
-        />
-
-      </div>
+      {isCalendar ? <Calendar/>:<ComponentA/>}
+      {/* <Calendar /> */}
     </div >
   )
+}
+
+const ComponentA = () => {
+  return (
+    <div>特になし</div>
+  )
+}
+
+const Calendar = () => {
+  return (
+    <div className='calendar'>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        weekends={true}
+        events={[
+          { title: '腕を休める期間', start: '2024-05-01', end: '2024-05-03' },
+          { title: '太ももを鍛える日', date: '2024-05-02' }
+        ]}
+        contentHeight={contentHeight}
+        dateClick={handleDateClick}
+        eventClick={handleEventClick}
+      // locales={[jaLocale]}
+      //日本語表記
+      />
+
+    </div>)
 }
