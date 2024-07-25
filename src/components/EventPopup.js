9b0f753bebe6fd77e307613eca6857a5074c3f74
cacaopony <shikoku.fumiya@gmail.com>
2024-07-25 11:41:35 +0900

    ポップアップウィンドウをドラッグできるように変更

--- src/components/EventPopup.js
+++ src/components/EventPopup.js
@@ -2,6 +2,27 @@ import { useState } from "react"
 
 const EventPopup = ({ onClose, onSave, onClick }) => {
     const [title, setTitle] = useState('');
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [isDragging, setIsDragging] = useState(false);
+    const [startPos, setstartPos] = useState({ x:0, y:0});
+
+    const handleMouseDown = (e) => {
+        setIsDragging(true);
+        setstartPos({ x:e.clientX - position.x, y:e.clientY-position.y});
+    }
+
+    const handleMouseMove = (e) => {
+        if(isDragging){
+            setPosition({
+                x:e.clientX - startPos.x,
+                y:e.clientY-startPos.y
+            })
+        }
+    }
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+    }
 
     const handleSave = () => {
         if (title) {
@@ -16,7 +37,13 @@ const EventPopup = ({ onClose, onSave, onClick }) => {
         <div>
             <div className="popup-overlay" onClick={onClick}>
             </div>
-            <div className="popup-content">
+            <div className="popup-content"
+                onClick={(e) => e.stopPropagation}
+                onMouseDown={handleMouseDown}
+                onMouseMove={handleMouseMove}
+                onMouseUp={handleMouseUp}
+                style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
+            >
                 <h3>Add Training Details</h3>
                 <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}
                     placeholder="Enter training details" />
